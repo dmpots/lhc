c22a23e01de04c62b1d9153639ebabf29dca8d6c
Austin Seipp <mad.one@gmail.com>
2008-11-23 11:11:30 -0600

    one more version notice update

--- cbits/_stdint.h
+++ cbits/_stdint.h
@@ -1,7 +1,7 @@
 #ifndef _JHC_CBITS__STDINT_H
 #define _JHC_CBITS__STDINT_H 1
 #ifndef _GENERATED_STDINT_H
-#define _GENERATED_STDINT_H "jhc 0.5.20080307"
+#define _GENERATED_STDINT_H "lhc 0.6.20081123"
 /* generated using gnu compiler gcc (GCC) 4.2.3 (Ubuntu 4.2.3-2ubuntu7) */
 #define _STDINT_HAVE_STDINT_H 1
 #include <stdint.h>
