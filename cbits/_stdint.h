89ce1b60b630393d05642c95e79738a2ed2bddef
Austin Seipp <mad.one@gmail.com>
2008-11-25 04:52:38 -0600

    removed cbits/

--- cbits/_stdint.h
+++
@@ -1,9 +0,0 @@
-#ifndef _LHC_CBITS__STDINT_H
-#define _LHC_CBITS__STDINT_H 1
-#ifndef _GENERATED_STDINT_H
-#define _GENERATED_STDINT_H "lhc 0.6.20081123"
-/* generated using gnu compiler gcc (GCC) 4.2.3 (Ubuntu 4.2.3-2ubuntu7) */
-#define _STDINT_HAVE_STDINT_H 1
-#include <stdint.h>
-#endif
-#endif
