ec24bc64bcba4035b08a27f37adbf62d43b5eab6
Austin Seipp <mad.one@gmail.com>
2008-12-09 22:04:07 -0600

    make 'cabal install lhc' install lhc's base package as well as the compiler (requires a Custom setup - see Setup.hs)

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,18 @@
+import System.Cmd
+import System.FilePath
 import Distribution.Simple
-main = defaultMain
+import Distribution.Simple.InstallDirs (CopyDest(..))
+import Distribution.Simple.LocalBuildInfo (absoluteInstallDirs, InstallDirs(..))
+
+main = defaultMainWithHooks simpleUserHooks { postInst = myPostInst }
+  where myPostInst _ _ pkgdesc buildinfo = do 
+          let dirs  = absoluteInstallDirs pkgdesc buildinfo NoCopyDest
+              datad = datadir dirs
+              lhc   = bindir dirs </> "lhc"
+              confargs = "--lhc --with-lhc="++lhc++" --prefix="++datad
+          installLhcPkgs confargs ["base"]
+        installLhcPkgs cf  = mapM_ (installLhcPkg cf)
+        installLhcPkg cf n = do putStrLn $ "\n[installing "++n++" package for lhc]\n"
+                                let dir = "lib" </> n
+                                system $ concat ["cd ",dir," && runghc Setup configure ",cf," && runghc Setup build && runghc Setup install"]
+                                return ()
