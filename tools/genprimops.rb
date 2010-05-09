d9445862479a28f8cbd180607b3c6d328371831d
David M Peixotto <dmp@rice.edu>
2010-05-09 14:45:47 -0500

    Group primops more accurately based on return type.

--- tools/genprimops.rb
+++ tools/genprimops.rb
@@ -1,36 +1,65 @@
 #!/usr/bin/ruby
 
-
-
-ghcBuiltins = []
-File.open("lib/ghc-prim/GHC/Prim.hs").each do |line|
-  if line =~ /#,$/ then
-    op = line.gsub(",","").lstrip.rstrip
-    ghcBuiltins << op
+class Prim
+  attr_accessor :name, :result
+  def initialize(name)
+    @name = name
+    @result = :unknown
   end
 end
 
+def error(msg)
+  puts "ERROR: #{msg}"
+  exit 1
+end
+def debug(msg)
+  #puts msg
+end
+def sanity(prims)
+  prims.values.each do |prim| 
+    if (prim.result != :vector && prim.result != :base) then
+      error("unknown prim type (#{prim.result}) for: #{prim.name}")
+    end
+  end
+end
+def addPrimType(prims, name, type)
+  prim = prims[name]
+  if prim.nil? then
+    error("trying to add type for unknown prim: #{name}")
+  elsif (prim.result != :unknown) then
+    error("multiple types for prim: #{name}")
+  else
+    prim.result = type
+  end
+end
 
-vectorBuiltins = %w(
-  unsafeFreezeByteArray#
-  newAlignedPinnedByteArray#
-  word2Integer#
-  integer2Int#
-  newByteArray#
-  newPinnedByteArray#
-                      
-  readInt8OffAddr#
-  readInt32OffAddr#
-  readAddrOffAddr#
-  readInt32OffAddr#
+PRIM_FILE = "lib/ghc-prim/GHC/Prim.hs" 
+prims = {}
+ghcBuiltins = []
+File.open(PRIM_FILE).each do |line|
+  if line =~ /#,$/ then
+    op = line.gsub(",","").lstrip.rstrip
+    ghcBuiltins << op
+    prims[op] = Prim.new(op)
+  end
+end
 
-  readWord8Array#
-  readDoubleOffAddr#
-  writeDoubleOffAddr#
-                        
-  mkWeak#
-  readCharArray#
-)
+File.open(PRIM_FILE).each do |line|
+  if line =~ /(\w+\#)\s+::\s+(.*)\#\)\s*$/ then
+    debug("V: #{line}")
+    addPrimType(prims, $1, :vector)
+  elsif line =~ /(\w+\#)\s+::\s+(.*)$/ then
+    debug("B: #{line}")
+    addPrimType(prims, $1, :base)
+  elsif line =~ /data (\w+\#)(\s*[a-z])*$/ then
+    debug("C: #{line}")
+    addPrimType(prims, $1, :base)
+  else
+    debug("JUNK: #{line}")
+  end
+end
+
+sanity(prims)
 
 unsupportedBuiltins = %w(
   raise#
@@ -41,7 +70,7 @@ unsupportedBuiltins = %w(
   atomically#
 )
 
-specialPrims = %w(
+specialBuiltins = %w(
   makeStablePtr#
   deRefStablePtr#
   unblockAsyncExceptions#
@@ -75,8 +104,14 @@ extraBuiltins = %w(
   realWorld#
 )
 
+specialCases   = specialBuiltins + unsupportedBuiltins
+bb             = prims.values.find_all {|p| p.result == :base}.map   {|p| p.name}
+baseBuiltins   = bb + extraBuiltins - specialCases
+vb             =  prims.values.find_all {|p| p.result == :vector}.map {|p| p.name}
+vectorBuiltins = vb - specialCases
 
 def printList(name, values)
+  puts "#{name} :: [CompactString]"
   puts "#{name} = ["
   print "    "
   puts values.map{|v| v.sub(/^/, '"').sub(/$/, '"')}.join("\n  , ")
@@ -86,12 +121,9 @@ end
 puts "{-# LANGUAGE OverloadedStrings #-}"
 puts "module LHC.Prim where"
 puts "import CompactString"
-puts "allBuiltins, baseBuiltins, vectorBuiltins, unsupportedBuiltins :: [CompactString]"
-allBuiltins = ghcBuiltins + extraBuiltins
-printList("allBuiltins", allBuiltins)
-printList("baseBuiltins", allBuiltins - vectorBuiltins - unsupportedBuiltins - specialPrims)
-printList("vectorBuiltins", vectorBuiltins)
-printList("unsupportedBuiltins", unsupportedBuiltins)
+printList("baseBuiltins", baseBuiltins.sort)
+printList("vectorBuiltins", vectorBuiltins.sort)
+printList("unsupportedBuiltins", unsupportedBuiltins.sort)
 
 puts "{- ORIGINAL DEFINITIONS"
 puts <<ORIG
